c72329b23a90c9114027efdd85650becd5da3c54
LiamHod <canada8715@gmail.com>
2017-05-25 15:47:11 -0600

    Got blob to print out. So it is going from database to code at least

--- src/sample/ConnectionTest.java
+++ src/sample/ConnectionTest.java
@@ -1,4 +1,5 @@
 package sample;
+import java.io.InputStream;
 import java.sql.*;
 import javax.naming.*;
 import javax.sql.*;
@@ -9,7 +10,7 @@ import javax.sql.*;
  */
 public class ConnectionTest {
     public static void main(String[] args){
-        String url = "jdbc:mysql://localhost:3306/project1test?useSSL=false";
+        String url = "jdbc:mysql://localhost:3306/project1?useSSL=false";
         String username = "root";
         String password = "admin";
 
@@ -17,16 +18,18 @@ public class ConnectionTest {
             System.out.println("Database connected!");
             Statement stmt = connection.createStatement();
             //stmt.execute("SELECT * FROM tabletest");
-            ResultSet rs = stmt.executeQuery("SELECT * FROM tabletest");
-            System.out.println(stmt);
-            System.out.println(rs);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM document");
+            //System.out.println(stmt);
+            //System.out.println(rs);
             rs.next();
             String foundType = rs.getString(1);
             System.out.println(foundType);
             //rs.next();
             String foundType2 = rs.getString(2);
             System.out.println(foundType2);
-
+            Blob imageBlob = rs.getBlob(7);
+            //InputStream binaryStream = imageBlob.getBinaryStream(0,imageBlob.length());
+            System.out.println(imageBlob);
 
             stmt.close();
             connection.close();
