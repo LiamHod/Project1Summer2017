eeaa91a7b87a68ceba26bf72f0afc3d2509e0dff
LiamHod <canada8715@gmail.com>
2017-08-08 14:42:05 -0600

    Cleared password field after failed login, and attempted to add a second thread to handle the load of a preview image

--- src/project1/view/OverviewController.java
+++ src/project1/view/OverviewController.java
@@ -7,6 +7,7 @@ package project1.view;
 //        The Apache Software Foundation (http://www.apache.org/).
 
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
@@ -239,42 +240,43 @@ public class OverviewController{
      * Loads a file preview
      */
     private void previewFile() {
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            String prevQuery = "SELECT docfile FROM document WHERE iddocument = ?";
-            PreparedStatement ps = connection.prepareStatement(prevQuery);
-            ps.setInt(1,selFile.getDocid());
-            ResultSet rs = ps.executeQuery();
-            rs.next();
-            Blob imageBlob = rs.getBlob(1);
-            InputStream is = imageBlob.getBinaryStream();
-            BufferedImage image = ImageIO.read(is);
-            Image disimage = SwingFXUtils.toFXImage(image, null);
-            double imheight = disimage.getHeight();
-            double imwidth = disimage.getWidth();
-            ImageView imageView = new ImageView(disimage);
-            AnchorPane previewPage = new AnchorPane();
-            previewPage.getChildren().addAll(imageView);
-            Scene scene = new Scene(previewPage,imwidth,imheight);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setTitle(selFile.getDocname());
-            stage.show();
-        } catch (IOException e) {
-            System.out.print("IOException");
-            Alert sqlAlert = new Alert(Alert.AlertType.ERROR);
-            sqlAlert.setTitle("Error previewing image");
-            sqlAlert.setHeaderText(null);
-            sqlAlert.setContentText("The program encountered an error and couldn't preview the image, check your connection and please try again");
-            sqlAlert.showAndWait();
-        } catch (SQLException e) {
-            Alert sqlAlert = new Alert(Alert.AlertType.ERROR);
-            sqlAlert.setTitle("Error previewing image");
-            sqlAlert.setHeaderText(null);
-            sqlAlert.setContentText("The program encountered an error and couldn't preview the image, check your connection and please try again");
-            sqlAlert.showAndWait();
-            throw new IllegalStateException("Cannot connect the database!", e);
-        }
-
+        Platform.runLater(() -> {
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                String prevQuery = "SELECT docfile FROM document WHERE iddocument = ?";
+                PreparedStatement ps = connection.prepareStatement(prevQuery);
+                ps.setInt(1, selFile.getDocid());
+                ResultSet rs = ps.executeQuery();
+                rs.next();
+                Blob imageBlob = rs.getBlob(1);
+                InputStream is = imageBlob.getBinaryStream();
+                BufferedImage image = ImageIO.read(is);
+                Image disimage = SwingFXUtils.toFXImage(image, null);
+                double imheight = disimage.getHeight();
+                double imwidth = disimage.getWidth();
+                ImageView imageView = new ImageView(disimage);
+                AnchorPane previewPage = new AnchorPane();
+                previewPage.getChildren().addAll(imageView);
+                Scene scene = new Scene(previewPage, imwidth, imheight);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.setTitle(selFile.getDocname());
+                stage.show();
+            } catch (IOException e) {
+                System.out.print("IOException");
+                Alert sqlAlert = new Alert(Alert.AlertType.ERROR);
+                sqlAlert.setTitle("Error previewing image");
+                sqlAlert.setHeaderText(null);
+                sqlAlert.setContentText("The program encountered an error and couldn't preview the image, check your connection and please try again");
+                sqlAlert.showAndWait();
+            } catch (SQLException e) {
+                Alert sqlAlert = new Alert(Alert.AlertType.ERROR);
+                sqlAlert.setTitle("Error previewing image");
+                sqlAlert.setHeaderText(null);
+                sqlAlert.setContentText("The program encountered an error and couldn't preview the image, check your connection and please try again");
+                sqlAlert.showAndWait();
+                throw new IllegalStateException("Cannot connect the database!", e);
+            }
+        });
     }
 
     /**
